b5599582dfcdaba0073560537b860f1522e7e544
WagnerManxa <wagnercordeiro@alunos.utfpr.edu.br>
2023-10-17 14:51:02 -0300

    1° Commit

---
+++ src/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Entrega {
+	requires java.desktop;
+	requires org.json;
+	requires com.fasterxml.jackson.databind;
+	requires java.sql;
+	requires org.apache.commons.codec;
+	requires jjwt.api;
+	requires jbcrypt;
+}
